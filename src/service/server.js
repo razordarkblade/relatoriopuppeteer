56cb43085ad92d3d37306d8a8e4cfd19ebb21fc7
Joao Benites <joao.alvesbenites@gmail.com>
2020-01-27 14:46:00 -0300

    Padronizaçao de projeto

--- src/service/server.js
+++ src/service/server.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +10,7 @@ app.use(cors());
 
 app.use('/',require('./routes'))
 
-var server = app.listen(PORT,HOST () =>{
+var server = app.listen((process.env.PORT || 4006), () =>{
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listeneing at http://%s:%s', host, port);
